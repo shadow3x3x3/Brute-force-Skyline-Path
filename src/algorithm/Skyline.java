28c7d1a698c080f677d20d5ef6f53c25c1924253
邱聖崴(qiu sheng wei) <shadow3x3x3@gmail.com>
2016-09-13 10:59:05 +0800

    Clear logic in dominate

--- src/algorithm/Skyline.java
+++ src/algorithm/Skyline.java
@@ -48,14 +48,14 @@ public class Skyline {
         int flag = 0;
         int checkFlag = path1Attrs.size();
 
-        if (path1Attrs.size() != path2Attrs.size()) {
-            throw new InvalidParameterException("path1 and path2's size are not the Same!");
-        } else {
+        if (path1Attrs.size() == path2Attrs.size()) {
             for (int i = 0; i < path1Attrs.size(); i++) {
                 flag = (path1Attrs.get(i) > path2Attrs.get(i)) ? flag + 1 : flag;
                 flag = (path1Attrs.get(i) < path2Attrs.get(i)) ? flag - 1 : flag;
                 checkFlag = (Objects.equals(path1Attrs.get(i), path2Attrs.get(i))) ? checkFlag - 1 : checkFlag;
             }
+        } else {
+            throw new InvalidParameterException("path1 and path2's size are not the Same!");
         }
 
         if (flag == checkFlag) {
